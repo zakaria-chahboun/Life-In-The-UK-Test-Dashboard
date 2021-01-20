4d3de18fca65f15a88992970d63b5fc7ca38e8bc
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2021-01-20 20:46:14 +0100

    Add section 2 for Tags handling

---
+++ src/AddTags.svelte
@@ -0,0 +1,168 @@
+<script>
+  import {
+    Field,
+    Input,
+    Tag,
+    Taglist,
+    Button,
+    Notification,
+    Message,
+    Progress,
+  } from "svelma";
+  import { onMount } from "svelte";
+  import { firestore } from "./firebase.js";
+
+  // local variables
+  let tag;
+  let tags = [];
+  let notification = {
+    showUp: false,
+    message: "",
+    type: "is-warning",
+  };
+
+  // ui/ux
+  let isLoading = false; // for push
+  let isLoadingTags = false; // for load
+
+  // Process tags, and  push it to firebase
+  async function addTag() {
+    // ux
+    isLoading = true;
+    // remove white spaces, tabs ..
+    tag = tag.trim();
+    // we don't want empty data
+    if (tag === "") {
+      isLoading = false;
+      return;
+    }
+    // we don't want duplicated data
+    else if (tags.includes(tag)) {
+      isLoading = false;
+      return;
+    }
+    // if there is many tags in input (sport,manga ..) add them all 👍
+    else if (tag.split(",").length > 1) {
+      tag = tag
+        .split(",")
+        .map((e) => e.trim())
+        .filter((e) => !tags.includes(e)); // avoid duplication tags in this case too 👍
+      tags = [...tags, ...tag];
+      tag = ""; // reset
+    }
+    // just one tag in input
+    else {
+      tags = [...tags, tag];
+      tag = ""; // reset
+    }
+    // -- finally push to firebase
+    try {
+      const _tags = await firestore
+        .collection("internals")
+        .doc("questions")
+        .set({ tags: tags });
+    } catch (error) {
+      loadTags(); // load the old tags from db
+      setnotification({ message: error });
+    } finally {
+      // ux
+      isLoading = false;
+    }
+  }
+  // Detele tags from ui & firebase
+  async function deleteTag(index) {
+    isLoadingTags = true;
+    tags.splice(index, 1);
+    tags = tags;
+    // -- finally push to firebase
+    try {
+      const _tags = await firestore
+        .collection("internals")
+        .doc("questions")
+        .set({ tags: tags });
+    } catch (error) {
+      loadTags(); // load the old tags from db
+      setnotification({ message: error });
+    } finally {
+      // ux
+      isLoadingTags = false;
+    }
+  }
+
+  // Show notifications
+  function setnotification({ message, type = "is-warning", timeout = 4000 }) {
+    notification.showUp = true; // show it
+    notification.message = message;
+    notification.type = type;
+
+    setTimeout(() => {
+      notification.showUp = false; // close it
+      notification.message = ""; // clear
+    }, timeout);
+  }
+
+  // Load tags from firebase
+  async function loadTags() {
+    isLoadingTags = true; // ux 😉
+    try {
+      const _tags = await firestore
+        .collection("internals")
+        .doc("questions")
+        .get();
+      isLoadingTags = false; // ux 😉
+      // check if 'tags' array exist in db
+      if (_tags.data().tags) tags = _tags.data().tags;
+    } catch (error) {
+      setnotification({
+        message: error,
+        type: "is-danger",
+      });
+      isLoadingTags = false; // ux 😉
+    }
+  }
+
+  // First
+  onMount(async () => {
+    loadTags();
+  });
+</script>
+
+<!-- Add tag field -->
+<label class="label">Push tags in database!</label>
+<Field>
+  <Input
+    expanded
+    placeholder="Insert 1 tag name like (sport) or multi-tags like (sport,history..) 🧒"
+    bind:value={tag}
+    on:keypress={(event) => {
+      if (event && event.key === 'Enter') addTag();
+    }}
+    icon="fire" />
+  <p class="control">
+    <Button type="is-dark" on:click={addTag} loading={isLoading}>Push!</Button>
+  </p>
+</Field>
+
+<!-- Hiding area; Notification for success of error -->
+<Notification
+  icon={true}
+  type={notification.type}
+  bind:active={notification.showUp}>
+  {notification.message}
+</Notification>
+
+<!-- Display tags -->
+<Taglist>
+  {#each tags as item, i}
+    <Tag closable type="is-dark" size="is-large" on:close={(e) => deleteTag(i)}>
+      {item}
+    </Tag>
+  {/each}
+</Taglist>
+
+<!-- Case: No tags in databse -->
+{#if isLoadingTags}
+  <Progress />
+{:else if tags.length == 0}
+  <Message title="Sorry">No Data In Firebase 😭</Message>
+{/if}
