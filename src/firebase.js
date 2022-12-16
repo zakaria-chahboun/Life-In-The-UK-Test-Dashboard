b88a618a67cedae842e206c710e2e0121b74cf9a
zakaria chahboun <zakaria.chahboun.1996@gmail.com>
2022-12-16 21:56:52 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmEgGH3X-eKHVKU48tAJ2mWTIcFrbpjQE",
-  authDomain: "quiz-e9e49.firebaseapp.com",
-  databaseURL: "https://quiz-e9e49.firebaseio.com",
-  projectId: "quiz-e9e49",
-  storageBucket: "quiz-e9e49.appspot.com",
-  messagingSenderId: "374320758324",
-  appId: "1:374320758324:web:caa79e93fe37be198f9461",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
