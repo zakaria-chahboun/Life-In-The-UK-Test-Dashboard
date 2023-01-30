df2cbf7ec597931f46701139f92793439c4e0dd9
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2023-01-30 17:16:49 +0100

    update

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDE0MzIwMSwiZXhwIjoxOTQ1NzE5MjAxfQ.CsonEZq7BbELLr2Jvqtv985ZiEFbzidGBCEfEtEk2L0'
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDE0MzIwMSwiZXhwIjoxOTQ1NzE5MjAxfQ.CsonEZq7BbELLr2Jvqtv985ZiEFbzidGBCEfEtEk2L0";
+const SUPABASE_URL = "https://glkylfhlgbxdibbqaqyg.supabase.co";
 
-const supabaseUrl = 'https://glkylfhlgbxdibbqaqyg.supabase.co'
-const supabase = createClient(supabaseUrl, SUPABASE_KEY)
-
-export { supabase };
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
